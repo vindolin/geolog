847d3a46fcf61babcf231606b80968521f2c984e
Thomas Schüßler <v@atomar.de>
2024-02-23 16:36:23 +0100

    comments

--- geolog/geolog.go
+++ geolog/geolog.go
@@ -14,13 +14,6 @@ import (
 	"github.com/oschwald/maxminddb-golang"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
-
-var throttler = NewIPThrottler()
-
 type mmrecord struct {
 	Location struct {
 		Latitude  float64 `maxminddb:"latitude"`
@@ -28,6 +21,14 @@ type mmrecord struct {
 	} `maxminddb:"location"`
 }
 
+var throttler = NewIPThrottler()
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+// parses an IP address from the beginning of a line
 func parseIp(line string) (net.IP, error) {
 	ipregexp := regexp.MustCompile(`\d+\.\d+\.\d+\.\d+`)
 	ipstr := ipregexp.FindString(line)
@@ -36,6 +37,7 @@ func parseIp(line string) (net.IP, error) {
 		return nil, fmt.Errorf("failed to find IP address")
 	}
 
+	// parse the IP address
 	ipAddr := net.ParseIP(ipstr)
 	if ipAddr == nil {
 		return nil, fmt.Errorf("failed to parse IP address")
@@ -44,6 +46,7 @@ func parseIp(line string) (net.IP, error) {
 	return ipAddr, nil
 }
 
+// handler is the main websocket handler
 func handler(w http.ResponseWriter, r *http.Request, tail *tail.Tail, gdb *maxminddb.Reader) {
 	log.Println("New connection")
 	conn, err := upgrader.Upgrade(w, r, nil)
@@ -52,6 +55,9 @@ func handler(w http.ResponseWriter, r *http.Request, tail *tail.Tail, gdb *maxmi
 		return
 	}
 
+	defer conn.Close()
+
+	// read lines from the log file
 	for line := range tail.Lines {
 		ip, _ := parseIp(line.Text)
 
@@ -61,10 +67,12 @@ func handler(w http.ResponseWriter, r *http.Request, tail *tail.Tail, gdb *maxmi
 
 		var ipAddr = ip.String()
 
+		// throttle requests from the same IP
 		if !throttler.Allow(ipAddr) {
 			continue
 		}
 
+		// lookup the IP address in the maxmind db
 		var record mmrecord
 		err = gdb.Lookup(ip, &record)
 		if err != nil {
@@ -72,9 +80,11 @@ func handler(w http.ResponseWriter, r *http.Request, tail *tail.Tail, gdb *maxmi
 			return
 		}
 
+		// format payload
 		log.Printf("ip: %s, lat: %f, long: %f\n", ip, record.Location.Latitude, record.Location.Longitude)
 		var payload = fmt.Sprintf("[%s, %f, %f]", ip, record.Location.Latitude, record.Location.Longitude)
 
+		// send the IP address and location to the client
 		err = conn.WriteMessage(websocket.TextMessage, []byte(payload))
 		if err != nil {
 			log.Println(err)
@@ -84,6 +94,7 @@ func handler(w http.ResponseWriter, r *http.Request, tail *tail.Tail, gdb *maxmi
 }
 
 func main() {
+	// setup command line arguments
 	parser := argparse.NewParser("run", "run the geolog websocket server")
 	logFile := parser.String("l", "log_file", &argparse.Options{Required: true, Help: "log file to tail"})
 	geoliteDb := parser.String("g", "geodb_file", &argparse.Options{Required: true, Help: "geolite db to use"})
@@ -94,6 +105,7 @@ func main() {
 		os.Exit(1)
 	}
 
+	// open the maxmind db
 	gdb, err := maxminddb.Open(*geoliteDb)
 	if err != nil {
 		log.Fatalf("failed to open maxmind db: %v", err)
@@ -101,6 +113,7 @@ func main() {
 	}
 	defer gdb.Close()
 
+	// tail the log file
 	tail, err := tail.TailFile(*logFile, tail.Config{
 		Follow: true,
 		ReOpen: true,
@@ -109,14 +122,18 @@ func main() {
 		log.Fatal(err)
 	}
 
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		handler(w, r, tail, gdb)
-	})
-
+	// serve the index.html file
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
 	})
 
+	// serve the websocket
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		handler(w, r, tail, gdb)
+	})
+
 	log.Println("Listening on :8080")
+
+	// start the http server
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
