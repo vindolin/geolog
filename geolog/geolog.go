08d0773350ecab4d35f501a5de072bf4e354d510
Thomas Schüßler <v@atomar.de>
2024-02-23 17:12:17 +0100

    use Pool to service multiple clients

--- geolog/geolog.go
+++ geolog/geolog.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"regexp"
+	"sync"
 
 	"github.com/akamensky/argparse"
 	"github.com/gorilla/websocket"
@@ -22,11 +23,51 @@ type mmrecord struct {
 	}
 }
 
-var throttler = NewIPThrottler()
-
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+}
+
+type Pool struct {
+	mu        sync.Mutex
+	clients   map[*websocket.Conn]bool
+	broadcast chan string
+}
+
+func NewPool() *Pool {
+	return &Pool{
+		clients:   make(map[*websocket.Conn]bool),
+		broadcast: make(chan string),
+	}
+}
+
+func (p *Pool) Add(conn *websocket.Conn) {
+	p.mu.Lock()
+	p.clients[conn] = true
+	p.mu.Unlock()
+}
+
+func (p *Pool) Remove(conn *websocket.Conn) {
+	p.mu.Lock()
+	delete(p.clients, conn)
+	p.mu.Unlock()
+}
+
+func (p *Pool) Broadcast(ip string) {
+	p.broadcast <- ip
+}
+
+func (p *Pool) Start() {
+	for {
+		ip := <-p.broadcast
+		for client := range p.clients {
+			err := client.WriteMessage(websocket.TextMessage, []byte(ip))
+			if err != nil {
+				log.Println(err)
+				p.Remove(client)
+			}
+		}
+	}
 }
 
 // parses an IP address from the beginning of a line
@@ -48,50 +89,14 @@ func parseIp(line string) (net.IP, error) {
 }
 
 // handler is the main websocket handler
-func handler(w http.ResponseWriter, r *http.Request, tail *tail.Tail, gdb *maxminddb.Reader) {
+func handler(w http.ResponseWriter, r *http.Request, pool *Pool) {
 	log.Println("New connection")
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-
-	defer conn.Close()
-
-	// read lines from the log file
-	for line := range tail.Lines {
-		ip, _ := parseIp(line.Text)
-
-		if ip == nil {
-			continue
-		}
-
-		var ipAddr = ip.String()
-
-		// throttle requests from the same IP
-		if !throttler.Allow(ipAddr) {
-			continue
-		}
-
-		// lookup the IP address in the maxmind db
-		var record mmrecord
-		err = gdb.Lookup(ip, &record)
-		if err != nil {
-			log.Printf("failed to lookup ip %s: %v", ip, err)
-			return
-		}
-
-		// format payload
-		log.Printf("ip: %s, lat: %f, long: %f\n", ip, record.Location.Latitude, record.Location.Longitude)
-		var payload = fmt.Sprintf("[%s, %f, %f]", ip, record.Location.Latitude, record.Location.Longitude)
-
-		// send the IP address and location to the client
-		err = conn.WriteMessage(websocket.TextMessage, []byte(payload))
-		if err != nil {
-			log.Println(err)
-			return
-		}
-	}
+	pool.Add(conn)
 }
 
 func main() {
@@ -125,6 +130,42 @@ func main() {
 		log.Fatal(err)
 	}
 
+	var throttler = NewIPThrottler()
+
+	pool := NewPool()
+	go pool.Start()
+
+	go func() {
+		// read lines from the log file
+		for line := range tail.Lines {
+			ip, _ := parseIp(line.Text)
+
+			if ip == nil {
+				continue
+			}
+
+			var ipAddr = ip.String()
+
+			// throttle requests from the same IP
+			if !throttler.Allow(ipAddr) {
+				continue
+			}
+
+			// lookup the IP address in the maxmind db
+			var record mmrecord
+			err = gdb.Lookup(ip, &record)
+			if err != nil {
+				log.Printf("failed to lookup ip %s: %v", ip, err)
+				return
+			}
+
+			// format payload
+			log.Printf("ip: %s, lat: %f, long: %f\n", ip, record.Location.Latitude, record.Location.Longitude)
+			var payload = fmt.Sprintf("[%s, %f, %f]", ip, record.Location.Latitude, record.Location.Longitude)
+			pool.broadcast <- payload
+		}
+	}()
+
 	// serve the index.html file
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
@@ -132,7 +173,7 @@ func main() {
 
 	// serve the websocket
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		handler(w, r, tail, gdb)
+		handler(w, r, pool)
 	})
 
 	log.Println("Listening on :" + *port)
